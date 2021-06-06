0e5e69bf5d284a050a41e58c8118c8b537fefebe
davidus27 <davidisbest27@gmail.com>
2021-06-06 18:31:41 +0200

    make link open on the new card

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
     return (
         <footer>
           {/* Footer containing attribution to the author of the logo design */}
-          <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+          <div>Icons made by <a href="https://www.freepik.com" target="_blank" rel="noreferrer" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" target="_blank" rel="noreferrer" title="Flaticon">www.flaticon.com</a>
           </div>
         </footer>
     );
--- src/components/NoDocumentFound.js
+++ src/components/NoDocumentFound.js
@@ -4,7 +4,7 @@ class NoDocumentFound extends Component {
   render() {
     return (
         <div id="pdf-not-found-div">
-            <div class="position-absolute top-0 start-50 translate-middle-x">
+            <div class="text-center position-absolute top-50 start-50 translate-middle">
                 <h1 class="text-center">No PDF document found on this site <span role="img" aria-label="Pensive Face" >😔</span></h1>
             </div>
         </div>
