e1fd042842c2fbbe54373c4458f786be7f42e9a7
davidus27 <davidisbest27@gmail.com>
2021-06-27 14:22:14 +0200

    added onFocusChanged listener

--- public/app/background/background.js
+++ public/app/background/background.js
@@ -1,10 +1,17 @@
 /*global chrome */
 
 // Called when the user clicks on the browser action
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.browserAction.onClicked.addListener(function() {
    // Send a message to the active tab
-   chrome.tabs.query({active: true, currentWindow:true},function(tabs) {
-        var activeTab = tabs[0];
+   chrome.tabs.query({active: true, currentWindow:true}, (tabs) => {
+        const activeTab = tabs[0];
         chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
    });
 });
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+   console.log('Focused changed!', windowId);
+   if (windowId === chrome.windows.WINDOW_ID_NONE) {
+      window.close();
+   }
+})
