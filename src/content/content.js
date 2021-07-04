c3ade055d2b79c2b54cc0285d9db76f90ba941d4
davidus27 <davidisbest27@gmail.com>
2021-07-04 22:05:55 +0200

    temporary remove iframe component

--- src/content/content.js
+++ src/content/content.js
@@ -2,30 +2,19 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Frame from 'react-frame-component';
-import App from "../App";
-
-class Main extends React.Component {
-    render() {
-      return (
-        <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
-          <App document={document} window={window} isExt={true}/> 
-        </Frame>
-    );
-    }
-}
+import App from "../components/App";
 
 const app = document.createElement('div');
 app.id = "my-extension-root";
 
 document.body.appendChild(app);
 ReactDOM.render(<App />, app);
-
 app.style.display = "none";
 
+
 chrome.runtime.onMessage.addListener(
-   function(request) {
-      if( request.message === "clicked_browser_action") {
+   (request) => {
+      if (request.message === "clicked_browser_action") {
         toggle();
       }
    }
